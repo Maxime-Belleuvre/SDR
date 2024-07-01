b7f65e876d3a797ca3326a6737f2851be9ddde48
Maxime-Belleuvre <102807688+Maxime-Belleuvre@users.noreply.github.com>
2024-07-01 18:54:31 +0200

    ban visuel

--- js/script.js
+++ js/script.js
@@ -5,48 +5,47 @@ function getData(url){
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data.class);
         createClassList(data.class);
         let banTeam1 =[];
         let banTeam2 =[];
 
         for(let i = 0; i < data.class.length; i++){
             let idClass = document.getElementById(i+1);
-            console.log(idClass)
+
             idClass.addEventListener("click", ()=>{
                 removeSelected(data.class);
                 idClass.classList.add("selected");
                 
             })
-            /* document.getElementById(`${i}`).addEventListener("click",() => document.getElementById(`${i}`).classList.add("selected")); */
         }
-        
+        document.getElementById("btnSubmitClass").addEventListener("click",()=> banClass(banTeam1,banTeam2,data.class))
     })
 }
 
 
 function createClassList(data){
-    for(let i=0; i<data.length; i++){
+    data.forEach(element => {
         let div = document.createElement("div");
+        let div2 = document.createElement("div");
         let img = document.createElement("img");
         let h2 = document.createElement("h2");
         
-        div.id = i+1;
+        div.id = element.id;
         div.classList.add("listClass__class");
         img.classList.add("listClass__class--img");
         h2.classList.add("listClass__class--name");
         
         
-        img.src = data[i].url;
-        h2.textContent = data[i].name;
+        img.src = element.url;
+        h2.textContent = element.name;
         
-
+        div.appendChild(div2);
         div.appendChild(img);
         div.appendChild(h2);
 
         document.getElementById("listClass").appendChild(div);
-  
-    };
+        
+    });
 }
 
 
@@ -55,9 +54,46 @@ function removeSelected(data){
         document.getElementById(i+1).classList.remove("selected");
     }
 }
-
-function banClass(arr1,arr2){
-
+function resetBanStyle(data){
+    for(let i=1; i<data.length+1; i++){
+        document.getElementById(i).classList.remove("classBan");
+    }
 }
+function banClass(arr1,arr2,data){
+    resetBanStyle(data);
+    if(arr1.length<=arr2.length){
+
+        arr2.forEach(element =>{
+            console.log(document.getElementById(element));
+            if(document.getElementById(element)){
+                document.getElementById(element).classList.add("classBan");
+            }
+        })
+        arr1.push(document.querySelector(".selected").getAttribute("id")) 
+    }else{
+        arr1.forEach(element =>{
+            if(document.getElementById(element)){
+                document.getElementById(element).classList.add("classBan");
+            }
+        })
+        arr2.push(document.querySelector(".selected").getAttribute("id"))
+    }
+
+
+
+    for(let i=1;i<arr1.length+1;i++){
+        document.getElementById(`team1Emplacement${i}`).src = data[arr1[i-1]-1].url;
+    }
+    for(let i=1;i<arr2.length+1;i++){
+        document.getElementById(`team2Emplacement${i}`).src = data[arr2[i-1]-1].url;
+    }
+    removeSelected(data)
+
+    console.log(arr1,arr2);
+}
+
+
+
+
 getData("json/class.json");
 
